726059a07baafdef1dda1939e4e0eb5572186938
Jamie Caesar <jamie.caesar@gmail.com>
2017-05-24 17:28:38 -0400

    Added dup intf detection and stub for port-chan checks

--- create_intf_desc.py
+++ create_intf_desc.py
@@ -37,42 +37,77 @@ from imports.cisco_securecrt import get_output
 from imports.cisco_tools import parse_with_textfsm
 from imports.cisco_tools import extract_system_name
 from imports.cisco_tools import short_int
+from imports.py_utils import human_sort_key
 
 
 ##################################  SCRIPT  ###################################
 
 
-def main():
-    """
-    Capture the CDP information from the connected device and ouptut it into a CSV file. 
-    """
-    send_cmd = "show cdp neighbors detail"
-
-    # Run session start commands and save session information into a dictionary
-    session = start_session(crt, script_dir)
-
-    # Capture output from show cdp neighbor detail
-    raw_cdp_list = get_output(session, send_cmd)
+def extract_cdp_data(raw_cdp):
 
     # TextFSM template for parsing "show cdp neighbor detail" output.  Must supply TextFSM template and output string.
     cdp_template = "textfsm-templates/show-cdp-detail"
     # Build path to template, process output and export to CSV
     template_path = os.path.join(script_dir, cdp_template)
-    cdp_table = parse_with_textfsm(raw_cdp_list, template_path)
+    cdp_table = parse_with_textfsm(raw_cdp, template_path)
 
-    # This will contain our configuration commands as CDP neighbors are found.
-    config_script = ""
+    cdp_data = {}
+    found_intfs = set()
 
-    # Generate a string of config commands to apply interface descriptions
-    for entry in cdp_table:
+    # Loop through all entry, excluding header row
+    for entry in cdp_table[1:]:
         local_intf = entry[0]
         device_id = entry[1]
         system_name = entry[2]
         remote_intf = entry[3]
         if system_name == "":
             system_name = extract_system_name(device_id)
-        config_script += "interface {}\n".format(local_intf)
-        config_script += "  description {}, {}\n".format(system_name, short_int(remote_intf))
+
+        # 7Ks can give multiple CDP entries when VDCs share the mgmt0 port.  If duplicate name is found, remove it
+        if local_intf in found_intfs:
+            # Remove from our description list
+            cdp_data.pop(system_name, None)
+        else:
+            cdp_data[local_intf] = (system_name, short_int(remote_intf))
+            found_intfs.add(local_intf)
+
+    return cdp_data
+
+
+# def add_port_channels(desc_data):
+#     pass
+
+
+def main():
+    """
+    Capture the CDP information from the connected device and ouptut it into a CSV file. 
+    """
+    # Run session start commands and save session information into a dictionary
+    session = start_session(crt, script_dir)
+
+    # Capture output from show cdp neighbor detail
+    raw_cdp_output = get_output(session, "show cdp neighbors detail")
+
+    description_data = extract_cdp_data(raw_cdp_output)
+
+    #TODO Add descriptions for port-channels where possible.
+    # # Capture port-channel output
+    # if session['OS'] == "NX-OS":
+    #     raw_pc_output = get_output(session, "show port-channel summary")
+    # elif "IOS" in session['OS']:
+    #     raw_pc_output = get_output(session, "show etherchannel summary")
+    # else:
+    #     raw_pc_output = ""
+    #
+    # add_port_channels(description_data)
+
+    # This will contain our configuration commands as CDP neighbors are found.
+    config_script = ""
+    # Generate a string of config commands to apply interface descriptions
+    intf_list = sorted(description_data.keys(), key=human_sort_key)
+    for interface in intf_list:
+        config_script += "interface {}\n".format(interface)
+        config_script += "  description {}, {}\n".format(description_data[interface][0], description_data[interface][1])
 
     output_filename = create_output_filename(session, "intf-desc", include_date=False)
     with open(output_filename, 'wb') as output_file:
