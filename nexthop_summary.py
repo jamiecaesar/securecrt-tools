fc3e511b9f1c949033da7cc075f85300e6a69caa
Jamie Caesar <jamie.caesar@gmail.com>
2017-07-20 00:14:49 -0400

    Updated nexthop_sum to use textfsm

--- nexthop_summary.py
+++ nexthop_summary.py
@@ -35,105 +35,160 @@ from imports.cisco_securecrt import end_session
 from imports.cisco_securecrt import create_output_filename
 from imports.cisco_securecrt import write_output_to_file
 
-from imports.cisco_tools import get_protocol
-from imports.cisco_tools import parse_routes
+from imports.cisco_tools import normalize_protocol
+from imports.cisco_tools import textfsm_parse_to_dict
+from imports.cisco_tools import update_empty_interfaces
 
-from imports.py_utils import read_file_to_list
 from imports.py_utils import list_of_lists_to_csv
 from imports.py_utils import human_sort_key
 
+import imports.google.ipaddress as ipaddress
 
 # #################################  SCRIPT  ###################################
 
-# TODO Re-write this script to use TextFSM directly, instead of intermediate conversion to a list of dicts.
 
-def nexthop_summary(route_list):
+def parse_routes(session, routes):
     """
-    This function will take the route_list datastructure (created by parse_routes) and process it into
-    a datastructure containing the summary data, that is then converted to a list so it can be easily written
-    into a CSV file (by list_to_csv).
-
-    :param route_list: A list of dictionaries - Each dictionary represents a single route entry.
-    :return: 
+    This function will take the raw route table from a devices (and a supported OS), process it with TextFSM, which
+    will return a list of lists.   Each sub-list in the TextFSM output represents a route entry.  Each of these entries
+    will be converted into a dictionary so that each item can be referenced by name (used in nexthop_summary)
+    :param session: Sessions data structure
+    :param routes: raw 'show ip route' output
+    :return: A list of dictionaries, with each dict representing a route.
     """
+    script_dir = session['settings']['script_dir']
+    if session['OS'] == "IOS":
+        template_file = os.path.join(script_dir, "textfsm-templates/show-ip-route-ios")
+    elif session['OS'] == "NX-OS":
+        template_file = os.path.join(script_dir, "textfsm-templates/show-ip-route-nxos")
+    else:
+        # If not a supported OS, return an empty list
+        return []
 
-    # There are 3 Dictionaries of information extracted from the route table:
-    # 1) Summary information - List each next-hop with how many routes from each
-    #    protocol
-    # 2) Connected Interfaces - List of the subnets that are on connected intfs
-    # 3) Detailed information - List of each next-hop and all the networks that
-    #    flow to that next-hop.
-    summary_dict = {}
-    connected_dict = {}
-    detail_dict = {}
+    # Normalize path before attempting to access (e.g. change slash to backslash for windows.)
+    template_file = os.path.normpath(template_file)
+    route_table = textfsm_parse_to_dict(routes, template_file)
 
-    for entry in route_list:
-        # Verify this entry has a next-hop parameter
-        proto = get_protocol(entry['protocol'])
-        if entry['nexthop'] and proto != "Connected":
-            nh = str(entry['nexthop'])
+    complete_table = []
+    for route in route_table:
+        new_entry = {}
 
-            if nh in summary_dict:
-                summary_dict[nh]['Total'] += 1
-                summary_dict[nh][proto] += 1
+        new_entry['network'] = ipaddress.ip_network(u"{0}/{1}".format(route['NETWORK'], route['MASK']))
+
+        new_entry['protocol'] = normalize_protocol(route['PROTOCOL'])
+
+        if route['NEXTHOP_IP'] == '':
+            new_entry['nexthop'] = None
+        else:
+            new_entry['nexthop'] = ipaddress.ip_address(unicode(route['NEXTHOP_IP']))
+
+        if route["NEXTHOP_IF"] == '':
+            new_entry['interface'] = None
+        else:
+            new_entry['interface'] = route['NEXTHOP_IF']
+
+        # Nexthop VRF will only occur in NX-OS route tables (%vrf-name after the nexthop)
+        if 'NEXTHOP_VRF' in route:
+            if route['NEXTHOP_VRF'] == '':
+                new_entry['vrf'] = None
             else:
-                summary_dict[nh] = {'int': entry['interface'],
-                                    'Total': 0,
-                                    'Static': 0,
-                                    'EIGRP': 0,
-                                    'OSPF': 0,
-                                    'BGP': 0,
-                                    'ISIS': 0,
-                                    'RIP': 0,
-                                    'Other': 0
-                                    }
-                summary_dict[nh]['Total'] += 1
-                summary_dict[nh][proto] += 1
-            if nh in detail_dict:
-                # Append the network and protocol (in a tuple)
-                detail_dict[nh].append((str(entry['network']), proto))
-            else:
-                # Create an entry for the next-hop and add network/proto to list.
-                detail_dict[nh] = [(str(entry['network']), proto)]
-        elif entry['interface']:
-            if entry['interface'] in connected_dict:
-                connected_dict[entry['interface']].append(str(entry['network']))
-            else:
-                connected_dict[entry['interface']] = [str(entry['network'])]
+                new_entry['vrf'] = route['NEXTHOP_VRF']
 
-    # Process Summary Data
-    nexthops = [['Next-hop', 'Interface', 'Total routes', 'Static', 'EIGRP', 'OSPF', 'BGP', 'ISIS', 'RIP', 'Other']]
-    # Put next-hop stats into list for writing into a CSV file
-    nexthops_data = []
-    for key, value in summary_dict.iteritems():
-        nexthops_data.append([key, value['int'], value['Total'], value['Static'], value['EIGRP'],
-                              value['OSPF'], value['BGP'], value['ISIS'], value['RIP'], value['Other']])
+        complete_table.append(new_entry)
 
-    # Append sorted nexthops stats after header line
-    nexthops.extend(sorted(nexthops_data, key=lambda x: human_sort_key(x[0])))
+    update_empty_interfaces(complete_table)
+    return complete_table
 
-    # Process Connected Network Data
-    connected = [['', ''], ['', ''], ['Connected', ''], ['Interface', 'Network(s)']]
-    conn_data = []
-    for key, value in connected_dict.iteritems():
-        this_row = [key]
-        this_row.extend(value)
-        conn_data.append(this_row)
-    connected.extend(sorted(conn_data, key=lambda x: human_sort_key(x[0])))
 
-    # Process Detailed Route Data
-    detailed = [['', '', ''], ['', '', ''], ['Route Details', '', ''], ['Next-Hop', 'Network(s)', 'Protocol']]
-    detail_data = []
-    sorted_keys = sorted(detail_dict, key=lambda x: human_sort_key(x))
-    for key in sorted_keys:
-        new_list = sorted(detail_dict[key], key=lambda x: human_sort_key(x[0]))
-        for entry in new_list:
-            this_row = [key, entry[0], entry[1]]
-            detail_data.append(this_row)
-        detail_data.extend([''])
-    detailed.extend(detail_data)
+def nexthop_summary(textfsm_dict):
+    # Identify connected or other local networks -- most found in NXOS to exlude from next-hops.  These are excluded
+    # from the nexthop summary (except connected has its own section in the output).
+    local_protos = ['connected', 'local', 'hsrp', 'vrrp', 'glbp']
 
-    return nexthops, connected, detailed
+    # Create a list of all dynamic protocols from the provided route table.  Add total and statics to the front.
+    proto_list = []
+    for entry in textfsm_dict:
+        if entry['protocol'] not in proto_list and entry['protocol'] not in local_protos:
+            proto_list.append(entry['protocol'])
+    proto_list.sort(key=human_sort_key)
+    proto_list.insert(0, 'total')
+    proto_list.insert(0, 'interface')
+
+    # Create dictionaries to store summary information as we process the route table.
+    summary_table = {}
+    connected_table = {}
+    detailed_table = {}
+
+    # Process the route table to populate the above 3 dictionaries.
+    for entry in textfsm_dict:
+        # If the route is connected, local or an FHRP entry
+        if entry['protocol'] in local_protos:
+            if entry['protocol'] == 'connected':
+                if entry['interface'] not in connected_table:
+                    connected_table[entry['interface']] = []
+                connected_table[entry['interface']].append(str(entry['network']))
+        else:
+            if entry['nexthop']:
+                if 'vrf' in entry and entry['vrf']:
+                    nexthop = "{0}%{1}".format(entry['nexthop'], entry['vrf'])
+                else:
+                    nexthop = str(entry['nexthop'])
+            elif entry['interface'].lower() == "null0":
+                nexthop = 'discard'
+
+            if nexthop not in summary_table:
+                # Create an entry for this next-hop, containing zero count for all protocols.
+                summary_table[nexthop] = {}
+                summary_table[nexthop].update(zip(proto_list, [0] * len(proto_list)))
+                summary_table[nexthop]['interface'] = entry['interface']
+            # Increment total and protocol specific count
+            summary_table[nexthop][entry['protocol']] += 1
+            summary_table[nexthop]['total'] += 1
+
+            if nexthop not in detailed_table:
+                detailed_table[nexthop] = []
+            detailed_table[nexthop].append((str(entry['network']), entry['protocol']))
+
+    # Convert summary_table into a format that can be printed to the CSV file.
+    output = []
+    header = ["Nexthop", "Interface", "Total"]
+    header.extend(proto_list[2:])
+    output.append(header)
+    summary_keys = sorted(summary_table.keys(), key=human_sort_key)
+    for key in summary_keys:
+        line = [key]
+        for column in proto_list:
+            line.append(summary_table[key][column])
+        output.append(line)
+    output.append([])
+
+    # Convert the connected_table into a format that can be printed to the CSV file (and append to output)
+    output.append([])
+    output.append(["Connected:"])
+    output.append(["Interface", "Network(s)"])
+    connected_keys = sorted(connected_table.keys(), key=human_sort_key)
+    for key in connected_keys:
+        line = [key]
+        for network in connected_table[key]:
+            line.append(network)
+        output.append(line)
+    output.append([])
+
+    # Convert the detailed_table into a format that can be printed to the CSV file (and append to output)
+    output.append([])
+    output.append(["Route Details"])
+    output.append(["Nexthop", "Network", "Protocol"])
+    detailed_keys = sorted(detailed_table.keys(), key=human_sort_key)
+    for key in detailed_keys:
+        for network in detailed_table[key]:
+            line = [key]
+            line.extend(list(network))
+            output.append(line)
+        output.append([])
+
+    # Return the output, ready to be sent to directly to a CSV file
+    return output
+
 
 
 def main():
@@ -162,16 +217,12 @@ def main():
 
         route_list = parse_routes(session, routes)
 
-        # Get a list of all nexthop stats as well as connected networks (2 lists).
-        nexthops, connected, detailed = nexthop_summary(route_list)
-        
-        # Merge the nexthops and connected interfaces into a single list before writing.
-        nexthops.extend(connected)
-        nexthops.extend(detailed)
-        
-        # Write data into a CSV file.
         output_filename = create_output_filename(session, "NextHopSummary", ext='.csv')
-        list_of_lists_to_csv(nexthops, output_filename)
+
+        # Process TextFSM output into list of lists (for direct output to CSV)
+        output_data = nexthop_summary(route_list)
+        # Write data into a CSV file.
+        list_of_lists_to_csv(output_data, output_filename)
     else:
         error_str = "This script does not support {}.\n" \
                     "It will currently only run on IOS Devices.".format(session['OS'])
