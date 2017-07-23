59041f4bf157652038f88af673659aa46de4b04e
Jamie Caesar <jamie.caesar@gmail.com>
2017-07-23 09:08:36 -0400

    Added arp to csv script

--- arp_to_csv.py
+++ arp_to_csv.py
@@ -2,10 +2,12 @@
 # $interface = "1.0"
 
 # ###############################  SCRIPT INFO  ################################
-# Author: XXXXXXXX
-# Email: XXXXXXX@presidio.com
+# Author: Jamie Caesar
+# Email: jcaesar@presidio.com
 # 
-# This script will grab the running configuration of a Cisco IOS or NX-OS device and save it into a file.
+# This script will capture the ARP table of the attached device and output the results as a CSV file.  While this
+# script can be used to capture the ARP table, the primary purpose is to create the ARP associations that the
+# "connected_ip.py" script can use to map which MAC and IP addresses are connected to each device.
 #
 
 # ##############################  SCRIPT SETTING  ###############################
@@ -39,27 +41,47 @@ if script_dir not in sys.path:
 # Imports from custom SecureCRT modules
 from imports.cisco_securecrt import start_session
 from imports.cisco_securecrt import end_session
+from imports.cisco_securecrt import get_output
+from imports.cisco_securecrt import create_output_filename
+from imports.cisco_securecrt import list_of_lists_to_csv
+
+from imports.cisco_tools import textfsm_parse_to_list
+
 
 
 # #################################  SCRIPT  ###################################
 
 
 def main():
-        """
-        Put a description of what the script will do here
-        """
+    """
+    Capture the ARP information from the connected device and ouptut it to a CSV file. 
+    """
+    send_cmd = "show ip arp"
 
-        # Run session start commands and save session information into a dictionary
-        session = start_session(crt, script_dir)
+    # Run session start commands and save session information into a dictionary
+    session = start_session(crt, script_dir)
 
-        # Make sure we completed session start.  If not, we'll receive None from start_session.
-        if session:
-            #
-            # PUT YOUR CODE HERE
-            #
+    # Make sure we completed session start.  If not, we'll receive None from start_session.
+    if session:
+        # Capture output from show cdp neighbor detail
+        raw_arp_list = get_output(session, send_cmd)
 
-            # Clean up before closing session
-            end_session(session)
+        # Parse CDP information into a list of lists.
+        # TextFSM template for parsing "show cdp neighbor detail" output
+        arp_template = "textfsm-templates/show-ip-arp-ios"
+        # Build path to template, process output and export to CSV
+        template_path = os.path.join(script_dir, arp_template)
+
+        # Use TextFSM to parse our output
+        cdp_table = textfsm_parse_to_list(raw_arp_list, template_path, add_header=True)
+
+        # Write TextFSM output to a .csv file.
+        output_filename = create_output_filename(session, "arp", ext=".csv")
+        list_of_lists_to_csv(session, cdp_table, output_filename)
+
+        # Clean up before exiting
+        end_session(session)
+
 
 if __name__ == "__builtin__":
     main()
