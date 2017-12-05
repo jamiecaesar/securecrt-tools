bb2c7532a10077408f96099437b868284db8d2a2
Jamie Caesar <jamie.caesar@gmail.com>
2017-12-05 14:57:50 -0500

    Added changes only + rollback

--- s_update_interface_desc.py
+++ s_update_interface_desc.py
@@ -24,7 +24,7 @@ logger.debug("Starting execution of {}".format(script_name))
 
 # ################################################   SCRIPT LOGIC   ###################################################
 
-def script_main(script):
+def script_main(script, prompt_checkmode=True, check_mode=True):
     """
     | SINGLE device script
     | Author: Jamie Caesar
@@ -42,6 +42,14 @@ def script_main(script):
     :param script: A subclass of the sessions.Session object that represents this particular script session (either
                     SecureCRTSession or DirectSession)
     :type script: script_types.Script
+    :param prompt_checkmode: A boolean that specifies if we should prompt the user to find out if we should run in
+        "check mode".  We would make this False if we were using this function in a multi-device script, so that the
+        process can run continually without prompting the user at each device.
+    :type prompt_checkmode: bool
+    :param check_mode: A boolean to specify whether we should run in "check mode" (Generate what the script would do
+        only -- does not push config), or not (Pushes the changes to the device).   The default is True for safety
+        reasons, and this option will be overwritten unless prompt_checkmode is False.
+    :type check_mode: bool
     """
     # Start session with device, i.e. modify term parameters for better interaction (assuming already connected)
     script.start_cisco_session()
@@ -52,20 +60,24 @@ def script_main(script):
         logger.debug("Unsupported OS: {0}.  Raising exception.".format(script.os))
         raise script_types.UnsupportedOSError("Remote device running unsupported OS: {0}.".format(script.os))
 
-    # Ask if this should be a test run (generate configs only) or full run (push updates to devices)
-    check_mode_message = "Do you want to run this script in check mode? (Only generate configs)\n" \
-                         "\n" \
-                         "Yes = Connect to device and write change scripts to a file ONLY\n" \
-                         "No = Connect to device and PUSH configuration changes"
-    message_box_design = script_types.ICON_QUESTION | script_types.BUTTON_YESNOCANCEL
-    logger.debug("Prompting the user to run in check mode.")
-    result = script.message_box(check_mode_message, "Run in Check Mode?", message_box_design)
-    if result == script_types.IDYES:
-        check_mode = True
-    elif result == script_types.IDNO:
-        check_mode = False
-    else:
-        return
+    if prompt_checkmode:
+        # Ask if this should be a test run (generate configs only) or full run (push updates to devices)
+        check_mode_message = "Do you want to run this script in check mode? (Only generate configs)\n" \
+                             "\n" \
+                             "Yes = Connect to device and write change scripts to a file ONLY\n" \
+                             "No = Connect to device and PUSH configuration changes"
+        message_box_design = script_types.ICON_QUESTION | script_types.BUTTON_YESNOCANCEL
+        logger.debug("Prompting the user to run in check mode.")
+        result = script.message_box(check_mode_message, "Run in Check Mode?", message_box_design)
+        if result == script_types.IDYES:
+            check_mode = True
+        elif result == script_types.IDNO:
+            check_mode = False
+        else:
+            return
+
+    # Get existing descriptions, so we don't push commands that would not change the description.
+    ex_desc_lookup = get_desc_table(script)
 
     # Get CDP Data
     raw_cdp = script.get_command_output("show cdp neighbors detail")
@@ -98,22 +110,46 @@ def script_main(script):
         pc_table = utilities.textfsm_parse_to_list(raw_pc_output, pc_template, add_header=True)
         add_port_channels(description_data, pc_table)
 
-    # Generate a list of configuration commands
+    # Create a list to append configuration commands and rollback commands
     config_commands = []
+    rollback = []
+
+     # Get an alphabetically sorted list of interfaces
     intf_list = sorted(description_data.keys(), key=utilities.human_sort_key)
+
+    # Generate a list of configuration commands (and rollback if necessary)
     for interface in intf_list:
-        config_commands.append("interface {}".format(interface))
+        # Get existing description
+        existing_desc = ex_desc_lookup[interface]
+
+        # If a port-channel only use hostname in description
         if "Po" in interface:
                 neigh_list = description_data[interface]
+                # If there is only 1 neighbor, use that
                 if len(neigh_list) == 1:
-                    config_commands.append(" description {}".format(neigh_list[0]))
+                    new_desc = neigh_list[0]
+                # If there are 2 neighbors, assume a vPC and label appropriately
                 if len(neigh_list) == 2:
                     neigh_list = sorted(neigh_list, key=utilities.human_sort_key)
-                    config_commands.append(" description vPC from {}, {}".format(neigh_list[0], neigh_list[1]))
+                    new_desc = "vPC from {}, {}".format(neigh_list[0], neigh_list[1])
+                # Only update description if we will be making a change
+                if new_desc != existing_desc:
+                    config_commands.append("interface {}".format(interface))
+                    config_commands.append(" description {}".format(new_desc))
+                    rollback.append("interface {}".format(interface))
+                    rollback.append(" description {}".format(existing_desc))
+
+        # For other interfaces, use remote hostname and interface
         else:
             remote_host = description_data[interface][0]
             remote_intf = utilities.short_int_name(description_data[interface][1])
-            config_commands.append(" description {}, {}".format(remote_host, remote_intf))
+            new_desc = "{}, {}".format(remote_host, remote_intf)
+            # Only update description if we will be making a change
+            if new_desc != existing_desc:
+                config_commands.append("interface {}".format(interface))
+                config_commands.append(" description {}".format(new_desc))
+                rollback.append("interface {}".format(interface))
+                rollback.append(" description {}".format(existing_desc))
 
     # If in check-mode, generate configuration and write it to a file, otherwise push the config to the device.
     if check_mode:
@@ -121,6 +157,7 @@ def script_main(script):
         with open(output_filename, 'wb') as output_file:
             for command in config_commands:
                 output_file.write("{}\n".format(command))
+        rollback_filename = script.create_output_filename("rollback", include_date=False)
     else:
         # Check settings to see if we prefer to save backups before/after applying changes
         take_backups = script.settings.getboolean(script_name, "take_backups")
@@ -129,18 +166,29 @@ def script_main(script):
             before_filename = script.create_output_filename("1-show-run-BEFORE")
             script.write_output_to_file("show run", before_filename)
             # Push configuration, capturing the configure terminal log
-            output_filename = before_filename.replace("1-show-run-BEFORE", "2-CONFIG-RESULTS")
+            output_filename = script.create_output_filename("2-CONFIG-RESULTS")
             script.send_config_commands(config_commands, output_filename)
             # Back up configuration after changes are applied
-            after_filename = before_filename.replace("1-show-run-BEFORE", "3-show-run-AFTER")
+            after_filename = script.create_output_filename("3-show-run-AFTER")
             script.write_output_to_file("show run", after_filename)
+            # Set Rollback filename, in case this option is used
+            rollback_filename = script.create_output_filename("4-ROLLBACK")
         else:
             # Push configuration, capturing the configure terminal log
             output_filename = script.create_output_filename("CONFIG-RESULTS")
             script.send_config_commands(config_commands, output_filename)
+            # Set Rollback filename, in case this option is used
+            rollback_filename = script.create_output_filename("ROLLBACK")
 
         # Save configuration
         script.save()
+
+    # Check our settings to see if we should create a rollback.
+    create_rollback = script.settings.getboolean(script_name, "rollback_file")
+    if create_rollback:
+        with open(rollback_filename, 'wb') as output_file:
+            for command in rollback:
+                output_file.write("{}\n".format(command))
 
     # Return terminal parameters back to the original state.
     script.end_cisco_session()
@@ -161,10 +209,10 @@ def extract_cdp_data(cdp_table):
 
     # Loop through all entry, excluding header row
     for entry in cdp_table[1:]:
-        local_intf = entry[0]
+        local_intf = utilities.long_int_name(entry[0])
         device_id = entry[1]
         system_name = entry[2]
-        remote_intf = entry[3]
+        remote_intf = utilities.long_int_name(entry[3])
         if system_name == "":
             system_name = utilities.extract_system_name(device_id)
 
@@ -203,7 +251,33 @@ def add_port_channels(desc_data, pc_data):
             desc_data[po_name] = list(neighbor_set)
 
 
+def get_desc_table(script):
+    """
+    A function that creates a lookup dictionary that can be used to get the description of an interface.
 
+    :param script: The script object that represents this script being executed
+    :type script: script_types.Script
+
+    :return: A dictionary that allows getting the description of an interface by using the interface as the key.
+    :rtype: dict
+    """
+    send_cmd = "show interface description"
+
+    if script.os == "IOS":
+        int_template = script.get_template("cisco_ios_show_interfaces_description.template")
+    else:
+        int_template = script.get_template("cisco_nxos_show_interface_description.template")
+
+    raw_int_desc = script.get_command_output(send_cmd)
+    desc_list = utilities.textfsm_parse_to_list(raw_int_desc, int_template)
+
+    desc_table = {}
+    # Change interface names to long versions for better matching with other outputs
+    for entry in desc_list:
+        intf = utilities.long_int_name(entry[0])
+        desc_table[intf] = entry[1]
+
+    return desc_table
 
 
 # ################################################  SCRIPT LAUNCH   ###################################################
