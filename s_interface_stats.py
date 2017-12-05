d4feafe7893319084a640a3d8883b9662872d58a
Jamie Caesar <jamie.caesar@gmail.com>
2017-12-04 21:04:42 -0500

    Converted interface stats script

---
+++ s_interface_stats.py
@@ -0,0 +1,84 @@
+# $language = "python"
+# $interface = "1.0"
+
+import os
+import sys
+import logging
+
+# Add script directory to the PYTHONPATH so we can import our modules (only if run from SecureCRT)
+if 'crt' in globals():
+    script_dir, script_name = os.path.split(crt.ScriptFullName)
+    if script_dir not in sys.path:
+        sys.path.insert(0, script_dir)
+else:
+    script_dir, script_name = os.path.split(os.path.realpath(__file__))
+
+# Now we can import our custom modules
+from securecrt_tools import script_types
+from securecrt_tools import utilities
+
+# Create global logger so we can write debug messages from any function (if debug mode setting is enabled in settings).
+logger = logging.getLogger("securecrt")
+logger.debug("Starting execution of {}".format(script_name))
+
+
+# ################################################   SCRIPT LOGIC   ###################################################
+
+def script_main(script):
+    """
+    SINGLE device script
+    Author: Jamie Caesar
+    Email: jcaesar@presidio.com
+
+    This script will scrape some stats (packets, rate, errors) from all the UP interfaces on the device and put it into
+    a CSV file.
+
+    :param script: A subclass of the sessions.Session object that represents this particular script session (either
+                    SecureCRTSession or DirectSession)
+    :type script: script_types.Script
+    """
+    # Create logger instance so we can write debug messages (if debug mode setting is enabled in settings).
+    logger = logging.getLogger("securecrt")
+    logger.debug("Starting execution of {}".format(script_name))
+
+    # Start session with device, i.e. modify term parameters for better interaction (assuming already connected)
+    script.start_cisco_session()
+
+    # Validate device is running a supported OS
+    supported_os = ["IOS", "NXOS"]
+    if script.os not in supported_os:
+        logger.debug("Unsupported OS: {0}.  Raising exception.".format(script.os))
+        raise script_types.UnsupportedOSError("Remote device running unsupported OS: {0}.".format(script.os))
+
+    send_cmd = "show interface"
+
+    # Get correct TextFSM template based on remote device OS
+    if script.os == "NXOS":
+        template_file = script.get_template("cisco_nxos_show_interface.template")
+    else:
+        template_file = script.get_template("cisco_ios_show_interfaces.template")
+
+    # Get raw output from the device
+    raw_intf = script.get_command_output(send_cmd)
+    # Process the output with TextFSM
+    fsm_results = utilities.textfsm_parse_to_list(raw_intf, template_file, add_header=True)
+    # Create output filename
+    output_filename = script.create_output_filename("int-stats", ext=".csv")
+    # Write output to a CSV file
+    utilities.list_of_lists_to_csv(fsm_results, output_filename)
+
+    # Return terminal parameters back to the original state.
+    script.end_cisco_session()
+
+
+# ################################################  SCRIPT LAUNCH   ###################################################
+
+# If this script is run from SecureCRT directly, use the SecureCRT specific class
+if __name__ == "__builtin__":
+    crt_script = script_types.CRTScript(crt)
+    script_main(crt_script)
+
+# If the script is being run directly, use the simulation class
+elif __name__ == "__main__":
+    direct_script = script_types.DirectScript(os.path.realpath(__file__))
+    script_main(direct_script)
