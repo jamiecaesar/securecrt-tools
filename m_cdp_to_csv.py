4657067dcce5b076297e1e223dc3b1b626fcdf3a
Jamie Caesar <jamie.caesar@gmail.com>
2017-12-13 09:48:48 -0500

    Added multi cdp script

--- m_cdp_to_csv.py
+++ m_cdp_to_csv.py
@@ -16,9 +16,6 @@ else:
 # Now we can import our custom modules
 from securecrt_tools import scripts
 from securecrt_tools import sessions
-from securecrt_tools import utilities
-# Import message box constants names for use specifying the design of message boxes
-from securecrt_tools.message_box_const import *
 import s_cdp_to_csv
 
 # Create global logger so we can write debug messages from any function (if debug mode setting is enabled in settings).
@@ -60,24 +57,24 @@ def script_main(script):
         return
 
     check_mode = True
-    # ##########################################  START CHECK MODE SECTION  ############################################
-    # Ask if this should be a test run (generate configs only) or full run (push updates to devices)
-    # Comment out or remove the entire CHECK MODE SECTION if you don't want to prompt for check mode
-    check_mode_message = "Do you want to run this script in check mode? (Only generate configs)\n" \
-                         "\n" \
-                         "Yes = Connect to device and write change scripts to a file ONLY\n" \
-                         "No = Connect to device and PUSH configuration changes"
-    message_box_design = ICON_QUESTION | BUTTON_YESNOCANCEL
-    logger.debug("Prompting the user to run in check mode.")
-    result = script.message_box(check_mode_message, "Run in Check Mode?", message_box_design)
-    if result == IDYES:
-        logger.debug("<M_SCRIPT> Received 'True' for Check Mode.")
-        check_mode = True
-    elif result == IDNO:
-        logger.debug("<M_SCRIPT> Received 'False' for Check Mode.")
-        check_mode = False
-    else:
-        return
+    # # #########################################  START CHECK MODE SECTION  ###########################################
+    # # Ask if this should be a test run (generate configs only) or full run (push updates to devices)
+    # # Comment out or remove the entire CHECK MODE SECTION if you don't want to prompt for check mode
+    # check_mode_message = "Do you want to run this script in check mode? (Only generate configs)\n" \
+    #                      "\n" \
+    #                      "Yes = Connect to device and write change scripts to a file ONLY\n" \
+    #                      "No = Connect to device and PUSH configuration changes"
+    # message_box_design = ICON_QUESTION | BUTTON_YESNOCANCEL
+    # logger.debug("Prompting the user to run in check mode.")
+    # result = script.message_box(check_mode_message, "Run in Check Mode?", message_box_design)
+    # if result == IDYES:
+    #     logger.debug("<M_SCRIPT> Received 'True' for Check Mode.")
+    #     check_mode = True
+    # elif result == IDNO:
+    #     logger.debug("<M_SCRIPT> Received 'False' for Check Mode.")
+    #     check_mode = False
+    # else:
+    #     return
     # ###########################################  END CHECK MODE SECTION  #############################################
 
     # ###########################################  START JUMP BOX SECTION  #############################################
@@ -170,19 +167,25 @@ def per_device_work(session, check_mode, enable_pass):
     task, it can be imported and called here, essentially making this script connect to all the devices in the chosen
     CSV file and then running a single-device script on each of them.
     """
-    s_cdp_to_csv.script_main(session.script)
+    s_cdp_to_csv.script_main(session)
 
 
 # ################################################  SCRIPT LAUNCH   ###################################################
 
 # If this script is run from SecureCRT directly, use the SecureCRT specific class
 if __name__ == "__builtin__":
+    # Initialize script object
     crt_script = scripts.CRTScript(crt)
+    # Run script's main logic against the script object
     script_main(crt_script)
+    # Shutdown logging after
     logging.shutdown()
 
 # If the script is being run directly, use the simulation class
 elif __name__ == "__main__":
+    # Initialize script object
     direct_script = scripts.DirectScript(os.path.realpath(__file__))
+    # Run script's main logic against the script object
     script_main(direct_script)
+    # Shutdown logging after
     logging.shutdown()
