b215dc1badcceedbe064aadbbf6c61f7c2d9c2b7
AirManic <grogier@cisco.com>
2020-04-29 11:32:19 -0500

    _TODO_AireOS.py notations

---
+++ _TODO_AireOS.py
@@ -0,0 +1,19 @@
+# $language = "python"
+# $interface = "1.0"
+
+    """
+    | SINGLE device script
+    | modified: Gordon Rogier grogier@cisco.com
+    |
+
+    # TODO show ap config general
+    # TODO show ap cdp neighbor all / show ap cdp neighbor detail
+    # TODO show advanced 802.11[a|b] txpower
+    # TODO show interface summary / show interface detail
+    # TODO show flexconnect summary / show flexconnect detail
+    # TODO show mobility summary
+
+    # TODO call various ap, cdp, int, flex, txpower
+    # TODO call show mobility summary, then connect to each WLC and call various ap, cdp, int, flex, txpower
+
+    """
